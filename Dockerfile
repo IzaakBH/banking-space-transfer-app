a870ee545f4b5cba9aaa770cd9067c11d38865a9
Izaak Birchall <izaakbirch@proton.me>
2025-10-04 17:50:39 +0100

    Make base url configurable by docker env values

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN npm install
 COPY . .
 RUN npm run build
 
+COPY env.sh /docker-entrypoint.d/env.sh
+RUN chmod +x /docker-entrypoint.d/env.sh
+
 FROM busybox:1.30 AS runner
 WORKDIR /app
 COPY --from=builder /app/dist .
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://plutus.15062000.xyz",
+  base: process.env.BASE_URL,
   server: {
     proxy: {
       '/live/api': {
