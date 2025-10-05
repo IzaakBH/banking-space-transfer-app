e3a0ba065563d9717ac4e32286d89ae1e357ef5a
Izaak Birchall <izaakbirch@proton.me>
2025-10-05 17:10:40 +0100

    Missing import

--- src/util/Utils.tsx
+++ src/util/Utils.tsx
@@ -1,3 +1,5 @@
+import { Amount } from '../App'
+
 export const formatAmount = (amount: Amount): string => {
     const value = (amount.minorUnits / 100).toFixed(2);
     return `${amount.currency} ${value}`;
